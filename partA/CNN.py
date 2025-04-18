13c80ca9f27f719fac405b18b6853644869b8be4
venkatesh19v <venkateshvenky1982002@gmail.com>
2025-04-18 17:09:28 +0530

    fix: test_step in CNN class

--- partA/CNN.py
+++ partA/CNN.py
@@ -86,6 +86,15 @@ class CNN(pl.LightningModule):
         acc = (logits.argmax(1) == y).float().mean()
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_acc", acc, prog_bar=True)
-
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        acc = (logits.argmax(1) == y).float().mean()
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", acc, prog_bar=True)
+        return loss
+    
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
